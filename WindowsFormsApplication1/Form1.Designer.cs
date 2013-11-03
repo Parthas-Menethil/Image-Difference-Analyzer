c783fdc0f2e7e8d8bbe265591febf57b982980ae
Parthas-Menethil <xywhpanda@gmail.com>
2013-11-03 14:00:48 -0500

    Initial Commit

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_convert = new System.Windows.Forms.Button();
+            this.lbl_timespan = new System.Windows.Forms.Label();
+            this.nud_offset = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nud_offset)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_convert
+            // 
+            this.btn_convert.Location = new System.Drawing.Point(12, 48);
+            this.btn_convert.Name = "btn_convert";
+            this.btn_convert.Size = new System.Drawing.Size(75, 23);
+            this.btn_convert.TabIndex = 0;
+            this.btn_convert.Text = "Convert";
+            this.btn_convert.UseVisualStyleBackColor = true;
+            this.btn_convert.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbl_timespan
+            // 
+            this.lbl_timespan.AutoSize = true;
+            this.lbl_timespan.Location = new System.Drawing.Point(12, 9);
+            this.lbl_timespan.Name = "lbl_timespan";
+            this.lbl_timespan.Size = new System.Drawing.Size(74, 13);
+            this.lbl_timespan.TabIndex = 1;
+            this.lbl_timespan.Text = "Process Time:";
+            // 
+            // nud_offset
+            // 
+            this.nud_offset.Location = new System.Drawing.Point(96, 20);
+            this.nud_offset.Name = "nud_offset";
+            this.nud_offset.Size = new System.Drawing.Size(120, 20);
+            this.nud_offset.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Allowed Offset:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(611, 417);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nud_offset);
+            this.Controls.Add(this.lbl_timespan);
+            this.Controls.Add(this.btn_convert);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nud_offset)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_convert;
+        private System.Windows.Forms.Label lbl_timespan;
+        private System.Windows.Forms.NumericUpDown nud_offset;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bitmap bit = (Bitmap)Bitmap.FromFile("E:\\WIN_20131026_231434.JPG");
+            lbl_timespan.Text = "Process Time:" + EdgeOptimize(bit, (int)nud_offset.Value).ToString();
+            bit.Save("E:\\refined.bmp");
+        }
+        private TimeSpan EdgeOptimize(Bitmap b){
+            Color c;
+            DateTime start_time = DateTime.Now;
+            for (int x = 0; x < b.Size.Width; x++){
+                for (int y = 0; y < b.Size.Height; y++){
+                    c = b.GetPixel(x, y);
+                    string hex = c.R.ToString("X2") + c.G.ToString("X2") + c.B.ToString("X2");
+                    int ret = Convert.ToInt32(hex, 16);
+                    if (ret <= 8388607){
+                        c = Color.FromArgb(0, 0, 0);
+                    }
+                    else{
+                        c = Color.FromArgb(255, 255, 255);
+                    }
+                    b.SetPixel(x, y, c);
+                }
+            }
+            return DateTime.Now - start_time;
+        }
+        private TimeSpan EdgeOptimize(Bitmap b, int ignore)
+        {
+            Color c;
+            ignore++;
+            DateTime start_time = DateTime.Now;
+            for (int x = 0; x <= b.Size.Width; x+=ignore)
+            {
+                for (int y = 0; y <= b.Size.Height; y+=ignore)
+                {
+                    c = b.GetPixel(x, y);
+                    string hex = c.R.ToString("X2") + c.G.ToString("X2") + c.B.ToString("X2");
+                    int ret = Convert.ToInt32(hex, 16);
+                    if (ret <= 8388607)
+                    {
+                        c = Color.FromArgb(0, 0, 0);
+                    }
+                    else
+                    {
+                        c = Color.FromArgb(255, 255, 255);
+                    }
+                    for (int tX = x; tX < x + ignore; tX++)
+                    {
+                        for (int tY = y; tY < y + ignore; tY++) {
+                            b.SetPixel(tX, tY, c);
+                        }
+                    }
+                        
+                }
+            }
+            return DateTime.Now - start_time;
+        }
+    }
+}
